5e2bec2e64f6c6e666d98353fb26065ab341f324
akulon23 <akulon2303@gmail.com>
2022-07-05 16:05:02 +0200

    Stworzenie ControllerInterface.

---
+++ src/Module/Application/Controller/ControllerInterface.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Application\Controller;
+
+/**
+ * Interface ControllerInterface
+ * @package Application\Controller
+ */
+interface ControllerInterface
+{
+    /**
+     * @return mixed
+     */
+    public function index();
+
+}
